3c7da44ce30053e8c279ec3ec36f8ab90de03904
jmlb23 <rmmenosrf@riseup.net>
2022-12-30 15:06:34 +0100

    - add swift and scala
    - add netrw minimal config

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "javascript", "typescript",  "lua", "rust" },
+  ensure_installed = { "c", "javascript", "typescript",  "lua", "rust", "swift", "scala"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
