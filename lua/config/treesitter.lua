52fb07b642eb0da700caa4a8139301153986dcea
Jesús López <jesus.lopez@trucksters.io>
2023-04-12 14:10:53 +0200

    add languages

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "javascript", "typescript",  "lua", "rust", "scala", "markdown"},
+  ensure_installed = { "c", "javascript", "typescript",  "lua", "python", "rust", "scala", "markdown"},
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
