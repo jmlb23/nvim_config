2c717f6f44ba76c602e0d9cdac2916dd14f4d5cf
jmlb23 <rmmenosrf@riseup.net>
2025-09-27 17:23:56 +0200

    set the tab to spaces crap

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -4,3 +4,8 @@ vim.keymap.set('n', '<Space>f', builtin.find_files, {})
 vim.keymap.set('n', '<Space>g', builtin.live_grep, {})
 --vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 --vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
