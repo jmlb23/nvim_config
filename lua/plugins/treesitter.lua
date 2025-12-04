ecbd9d5e44a3263c4fafc7e8a865f0968bf1583e
jmlb23 <rmmenosrf@riseup.net>
2025-12-04 19:34:37 +0100

    configure treesitter to autoinstall

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,15 @@
 return {
-    "nvim-treesitter/nvim-treesitter", 
-    branch = 'main', 
-    lazy = false, 
-    build = ":TSUpdate",
-}
+	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
+	lazy = false,
+	config = function()
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "*",
+			callback = function(args)
+				local lang = vim.treesitter.language.get_lang(args.match) or args.match
 
+				require("nvim-treesitter").install({ lang })
+			end,
+		})
+	end,
+}
