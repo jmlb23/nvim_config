c5e7bdfd2ff3a4379a3609672e986b33f068e934
jmlb23 <rmmenosrf@riseup.net>
2025-12-04 16:54:22 +0100

    add neotree as a filesystem tree viewer

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,8 @@ vim.keymap.set('n', '<C-f>', vim.lsp.buf.format, {
   desc = 'Format current buffer with LSP'
 })
 
+vim.keymap.set('n', '<A-t>', ":Neotree filesystem reveal left<CR>")
+
 vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
