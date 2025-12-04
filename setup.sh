3c0193943a239f2827b037eb48a05ff1d96fc4fd
jmlb23 <rmmenosrf@riseup.net>
2025-12-04 16:43:40 +0100

    remove packer installation

--- setup.sh
+++ setup.sh
@@ -4,14 +4,6 @@ command -v rg 2>&1 >/dev/null || echo "ripgrep is recomended to be installed"
 
 command -v yarn 2>&1 >/dev/null || echo "yarn is required to install tree-sitter"
 
-clone_packer()
-{
-    git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim >/dev/null 2>&1
-}
-
-command -v git >/dev/null 2>&1 && clone_packer || echo "Error cloning Packer"
-
-
 # install treesiter-cli
 
 command -v yarn >/dev/null 2>&1 && yarn global add tree-sitter-cli || echo "yarn is required to install tree-sitter"
