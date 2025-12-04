df713a113f85dcf2e37266e829529594a85260e9
jmlb23 <rmmenosrf@riseup.net>
2025-12-04 19:34:10 +0100

    move some things to their correct place and set other options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,1 +1,12 @@
 vim.opt.signcolumn = 'yes'
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
