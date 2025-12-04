45f649dddac2ab193af921a29e66cf482906c385
jmlb23 <rmmenosrf@riseup.net>
2025-12-04 17:13:00 +0100

    force hjkl

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,18 @@ vim.keymap.set('n', '<C-f>', vim.lsp.buf.format, {
   desc = 'Format current buffer with LSP'
 })
 
+
+local function disable_key(mode, key)
+  vim.keymap.set(mode, key, '<Nop>', { noremap = true, silent = true })
+end
+
+for _, mode in pairs({ 'n', 'i', 'v', 'c' }) do
+  disable_key(mode, '<Up>')
+  disable_key(mode, '<Down>')
+  disable_key(mode, '<Left>')
+  disable_key(mode, '<Right>')
+end
+
 vim.keymap.set('n', '<A-t>', ":Neotree filesystem reveal left<CR>")
 
 vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
