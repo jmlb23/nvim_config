6b3fb8d0a5984749ac1c56bd110ee87eb349cd7c
jmlb23 <rmmenosrf@riseup.net>
2026-02-22 11:28:28 +0100

    change filetype logic and ignore some types

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,8 +1,18 @@
+-- install treesitter for language if not installed
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "*",
-	callback = function(args)
-		local lang = vim.treesitter.language.get_lang(args.match) or args.match
-
-		require("nvim-treesitter").install({ lang })
-	end,
+    pattern = "*",
+    callback = function(args)
+        local lang = args.match
+        local unrecognised = {
+            ['neo-tree'] = true,
+            help = true,
+            gitcommit = true
+        }
+        if
+            not pcall(vim.treesitter.language.inspect, lang) and
+            not unrecognised[lang]
+        then
+            require("nvim-treesitter").install({ lang })
+        end
+    end,
 })
